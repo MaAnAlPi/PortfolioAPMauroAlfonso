f2a370ce6926bd44bff51eec6dfbab50c0d60733
Mauro <109828921+MaAnAlPi@users.noreply.github.com>
2022-11-30 03:20:51 -0300

    FrontEnd

---
+++ Portfolio/FrontEnd/src/app/components/logoap/logoap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logoap',
+  templateUrl: './logoap.component.html',
+  styleUrls: ['./logoap.component.css']
+})
+export class LogoAPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
